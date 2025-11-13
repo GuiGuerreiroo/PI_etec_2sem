6bd5f8549334a4f4d0328be4e31c8453fbecd839
Gabriel Bocato <gabrielbocato11@gmail.com>
2025-11-13 19:57:46 -0300

    feat: implement get all material to show up on edit material screen

--- front/src/app/api/material/get_all_materials.js
+++ front/src/app/api/material/get_all_materials.js
@@ -4,12 +4,12 @@ async function getAllMaterials() {
             `http://localhost:3000/api/materials`,
             {
                 headers: {
-                    Authorization: `Bearer ${JSON.parse(localStorage.getItem("token"))}`
+                    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4ZmJlZTkyNWY4ZmIxOGRiY2U4ZWNiOSIsInJvbGUiOiJBRE1JTiIsImV4cCI6MTc2MzE1Nzg0MSwiaWF0IjoxNzYzMDcxNDQxfQ.bQPgEUcwinU7d-Mh1YW33AWwcyFkYq5MAc7-bn-UXnI`
                 }
             } 
         );
         console.log("Resposta da API:");
-        console.log(response.data);
+        console.log(response.data.material);
         return response.data;
     }
     catch (error) {
--- front/src/app/scripts/edit_material.js
+++ front/src/app/scripts/edit_material.js
@@ -41,5 +41,45 @@ document.addEventListener('DOMContentLoaded', () => {
 
 async function loadMaterials() {
   const materials = await getAllMaterials();
-  console.log(materials);
+  console.log(materials.material);
+
+  const materialContainer = document.getElementById('materials-container');
+
+  materials.material.forEach((material) => {
+
+    const card = document.createElement('div');
+    card.className = 'col-12 col-sm-6 col-lg-4';
+
+    const itemCard = document.createElement('div');
+    itemCard.className = 'item-card';
+
+    const h3 = document.createElement('h3');
+    h3.innerText = `${material.name} ${material.size || ''}`;
+
+    const p = document.createElement('p');
+    p.innerText = 'Quantidade: ';
+
+    const b = document.createElement('b');
+    b.innerText = material.totalQuantity;
+
+    p.appendChild(b);
+
+    const editButton = document.createElement('button');
+    editButton.className = 'editar';
+    editButton.innerText = 'Editar';
+
+    itemCard.appendChild(h3);
+    itemCard.appendChild(p);
+    itemCard.appendChild(editButton);
+
+    card.appendChild(itemCard);
+    materialContainer.appendChild(card);
+  });
+  const plusCard = document.createElement('div');
+  plusCard.className = 'col-12 col-sm-6 col-lg-4';
+  const addCard = document.createElement('div');
+  addCard.className = 'add-card';
+  addCard.innerHTML = '<span>+</span>';
+  plusCard.appendChild(addCard);
+  materialContainer.appendChild(plusCard);
 }
