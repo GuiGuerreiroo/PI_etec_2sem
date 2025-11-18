e3b937cc169fca64f65ebee35591c61ce5e4dec2
Guilherme Guerreiro de Oliveira <155542954+GuiGuerreiroo@users.noreply.github.com>
2025-11-18 15:23:38 -0300

    fix: add the unmappeed hours enums also changed the get all materials response to be materials and on get all kits to be kits

--- back/app/modules/kit/get_all_kits/get_all_kits_schema.ts
+++ back/app/modules/kit/get_all_kits/get_all_kits_schema.ts
@@ -3,7 +3,7 @@ import { KitMongoDTO } from "../../../shared/repositories/database/mongo/kit_rep
 export async function getAllKitsResponse(kits: KitMongoDTO[]) {
     return {
         message: "Kits retornados com sucesso",
-        kit: kits.map((kit) => ({
+        kits: kits.map((kit) => ({
             id: kit.id,
             name: kit.name,
             materials: kit.materials,
